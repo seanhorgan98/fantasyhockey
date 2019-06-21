2d6459b59f184072b9c79aaccf204d92b8450700
Sean Horgan <seanhorgan98@gmail.com>
2019-06-22 00:17:42 +0100

    Added log in page with Google log-in functionality

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+// pages/home_page.dart
+import 'package:fantasy_hockey/main.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+
+  final String title;
+  HomePage(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+        backgroundColor: Colors.blue[700],
+      ),
+
+
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              LoginButton(),
+              UserProfile()
+            ],
+          ),
+        )
+      ),
+    );
+  }
+  
+}
