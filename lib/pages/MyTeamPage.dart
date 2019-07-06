cb39cdc5956b914c4ef3ec23ae5fd2b27a816f8e
Sam Owen-Hughes <samuelowenhughes@gmail.com>
2019-07-06 23:33:43 +0100

    add popup for player onclick

--- lib/pages/MyTeamPage.dart
+++ lib/pages/MyTeamPage.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MyTeamPage extends StatelessWidget{
+enum Response { Captain, Substitute, Stats, Transfer }
+
+class MyTeamPage extends StatefulWidget{
+  @override
+  MyTeamPageState createState() => new MyTeamPageState();
+}
+
+class MyTeamPageState extends State<MyTeamPage> {
   final MyGridView myGridView = new MyGridView();
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,47 +18,94 @@ class MyTeamPage extends StatelessWidget{
           backgroundColor: Colors.blue[700],
           title: new Text("My Team"),
         ),
-        body: myGridView.build(),
+        body: myGridView.build(context),
       );
   }
+
+
 }
 
 class MyGridView{
-  List players = ["Adshead", "Cabbage", "Horgan", "Montgomery", "SOH", "Bond"];
-  
-  Card getStructuredGridCell(name, color) {
-    return new Card(
-        elevation: 1.5,
-        color: color,
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          verticalDirection: VerticalDirection.down,
-          children: <Widget>[
-            new Center(
-              child: new Text(name),
-            )
-          ],
-        ));
+ //Filler Names, Will need to be loaded from DB
+  List players = ["Adshead", "Cabbage", "Horgan", "Montgomery", "SOH", "Bond", "TB3"];
+
+  Future<Response> _asyncSimpleDialog(context, name) async {
+    return await showDialog<Response>(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text(name),
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, Response.Captain);
+                },
+                child: const Text('Captain'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, Response.Substitute);
+                },
+                child: const Text('Substitute'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, Response.Stats);
+                },
+                child: const Text('Player Stats'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, Response.Transfer);
+                },
+                child: const Text('Transfer Out'),
+              ),
+            ],
+          );
+        });
   }
 
-  GridView build() {
+  RaisedButton getStructuredGridCell(name, color, context) {
+    return new RaisedButton(
+      elevation: 5,
+      color: color,
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        verticalDirection: VerticalDirection.down,
+        children: <Widget>[
+          new Center(
+            child: new Text(
+              name,
+              style: new TextStyle(fontSize: 25, color: Colors.black)                  
+            ),
+          ),
+        ],
+      ),
+      onPressed: () {_asyncSimpleDialog(context, name);}
+    );
+  }
+
+
+  GridView build(context) {
     return new GridView.count(
       primary: true,
       padding: const EdgeInsets.all(10.0),
       crossAxisCount: 2,
       childAspectRatio: 0.85,
-      mainAxisSpacing: 5.0,
-      crossAxisSpacing: 5,
+      mainAxisSpacing: 10.0,
+      crossAxisSpacing: 10,
       children: <Widget>[
-        //Filler Names, Will need to be loaded from DB
-        getStructuredGridCell(players[0], Colors.green),
-        getStructuredGridCell(players[1], Colors.green),
-        getStructuredGridCell(players[2], Colors.red),
-        getStructuredGridCell(players[3], Colors.red),
-        getStructuredGridCell(players[4], Colors.yellow),
-        getStructuredGridCell(players[5], Colors.yellow),
+        getStructuredGridCell(players[0], Colors.red, context),
+        getStructuredGridCell(players[1], Colors.red, context),
+        getStructuredGridCell(players[2], Colors.orange, context),
+        getStructuredGridCell(players[3], Colors.orange, context),
+        getStructuredGridCell(players[4], Colors.yellow, context),
+        getStructuredGridCell(players[5], Colors.yellow, context),
+        getStructuredGridCell(players[6], Colors.grey, context),
       ],
+      
     );
   }
 }
