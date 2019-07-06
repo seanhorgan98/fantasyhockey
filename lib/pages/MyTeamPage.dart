dce8ade5c92750aad6883909c96fed5d5b2236fe
Sam Owen-Hughes <samuelowenhughes@gmail.com>
2019-07-06 22:07:12 +0100

    Basic layout set

--- lib/pages/MyTeamPage.dart
+++ lib/pages/MyTeamPage.dart
@@ -1,18 +1,61 @@
 import 'package:flutter/material.dart';
 
 class MyTeamPage extends StatelessWidget{
+  final MyGridView myGridView = new MyGridView();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue[700],
-        title: Text("My Team"),
-      ),
-      body: Center(
-        
+    return new MaterialApp(
+     // debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.blue[700],
+          title: new Text("My Team"),
+        ),
+        body: myGridView.build(),
       ),
     );
   }
-  
 }
+
+class MyGridView{
+  List players = ["Adshead", "Cabbage", "Horgan", "Montgomery", "SOH", "Bond"];
+  
+  Card getStructuredGridCell(name, color) {
+    return new Card(
+        elevation: 1.5,
+        color: color,
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          verticalDirection: VerticalDirection.down,
+          children: <Widget>[
+            new Center(
+              child: new Text(name),
+            )
+          ],
+        ));
+  }
+
+  GridView build() {
+    return new GridView.count(
+      primary: true,
+      padding: const EdgeInsets.all(10.0),
+      crossAxisCount: 2,
+      childAspectRatio: 0.85,
+      mainAxisSpacing: 5.0,
+      crossAxisSpacing: 5,
+      children: <Widget>[
+        //Filler Names, Will need to be loaded from DB
+        getStructuredGridCell(players[0], Colors.green),
+        getStructuredGridCell(players[1], Colors.green),
+        getStructuredGridCell(players[2], Colors.red),
+        getStructuredGridCell(players[3], Colors.red),
+        getStructuredGridCell(players[4], Colors.yellow),
+        getStructuredGridCell(players[5], Colors.yellow),
+      ],
+    );
+  }
+}
+
 
