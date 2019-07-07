e00560f849ae05b1ba32fe15c5fd43c2d7b6bf2f
Sam Owen-Hughes <samuelowenhughes@gmail.com>
2019-07-07 02:36:34 +0100

    Finalised layout

--- lib/pages/MyTeamPage.dart
+++ lib/pages/MyTeamPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 enum Response { Captain, Substitute, Stats, Transfer }
 
 class MyTeamPage extends StatefulWidget{
@@ -8,27 +7,40 @@ class MyTeamPage extends StatefulWidget{
 }
 
 class MyTeamPageState extends State<MyTeamPage> {
-  final MyGridView myGridView = new MyGridView();
 
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: Colors.blue[700],
-          title: new Text("My Team"),
-        ),
-        body: myGridView.build(context),
-      );
-  }
-
-
-}
-
-class MyGridView{
- //Filler Names, Will need to be loaded from DB
+   //Filler Names, Will need to be loaded from DB
   List players = ["Adshead", "Cabbage", "Horgan", "Montgomery", "SOH", "Bond", "TB3"];
   int captain = 2;
+
+  ButtonTheme getStructuredGridCell(name, captain, color, context) {
+    if(captain){
+       name = name + "\n(Captain)";
+    }
+
+    return new ButtonTheme(
+      minWidth: 200,
+      height: 100,
+      child: RaisedButton(
+        elevation: 10,
+        color: color,
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          verticalDirection: VerticalDirection.down,
+          children: <Widget>[
+            new Center(
+              child: new Text(
+                name,
+                style: new TextStyle(fontSize: 25, color: Colors.black),
+                textAlign: TextAlign.center,                  
+              ),
+            ),
+          ],
+        ),
+        onPressed: () {_asyncSimpleDialog(context, name);}
+      )
+    );
+  }
 
   //On Button Click Popop
   //Not sure how to get this Promise
@@ -73,54 +85,80 @@ class MyGridView{
         });
   }
 
-  RaisedButton getStructuredGridCell(name, captain, color, context) {
-      if(captain){
-        name = name + "\n(Captain)";
-      }
-    return new RaisedButton(
-      
 
-      elevation: 5,
-      color: color,
-      child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        verticalDirection: VerticalDirection.down,
-        children: <Widget>[
-          new Center(
-            child: new Text(
-              name,
-              style: new TextStyle(fontSize: 25, color: Colors.black),
-              textAlign: TextAlign.center,                  
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.blue[700],
+          title: new Text("My Team"),
+        ),
+
+        body: SizedBox(
+          height: 700,
+          child: GridView.count(         
+          primary: true,
+          crossAxisCount: 1,
+          childAspectRatio: 4.5,
+          mainAxisSpacing: 0,
+          crossAxisSpacing: 20,
+          padding: const EdgeInsets.all(5.5),
+          children: <Widget>[
+            
+            //Transfer and points info
+            SizedBox(
+              height: 10,
+              child: Card(
+                color: Colors.purple,
+                child: Center( child: Text("Some info about team"),),
+              ),
             ),
-          ),
-        ],
-      ),
-      onPressed: () {_asyncSimpleDialog(context, name);}
+            
+
+            // Row for each position in team
+            Row(
+              children: <Widget>[
+                getStructuredGridCell(players[0], captain == 0, Colors.red, context),
+                getStructuredGridCell(players[1], captain == 1, Colors.red, context),
+              ],
+            ),
+            
+            Row(
+              children: <Widget>[
+                getStructuredGridCell(players[2], captain == 2, Colors.orange, context),
+                getStructuredGridCell(players[3], captain == 3, Colors.orange, context),
+              ],
+            ),
+
+            Row(
+              children: <Widget>[
+                getStructuredGridCell(players[4], captain == 4, Colors.yellow, context),
+                getStructuredGridCell(players[5], captain == 5, Colors.yellow, context),
+              ],
+            ),              
+            
+            // Substitute
+            Container( 
+              color: Colors.purple,
+              child: getStructuredGridCell(players[6], false, Colors.grey, context)
+            ),
+            
+            //Boosts
+            SizedBox(
+              height: 10,
+              child: Card(
+                color: Colors.purple,
+                child: Center( child: Text("Boosts"),),
+              ),
+            ),
+
+          ]  
+        )
+      )
     );
   }
 
 
-  GridView build(context) {
-    return new GridView.count(
-      primary: true,
-      padding: const EdgeInsets.all(10.0),
-      crossAxisCount: 2,
-      childAspectRatio: 0.85,
-      mainAxisSpacing: 10.0,
-      crossAxisSpacing: 10,
-      children: <Widget>[
-        getStructuredGridCell(players[0], captain == 0, Colors.red, context),
-        getStructuredGridCell(players[1], captain == 1, Colors.red, context),
-        getStructuredGridCell(players[2], captain == 2,Colors.orange, context),
-        getStructuredGridCell(players[3], captain == 3, Colors.orange, context),
-        getStructuredGridCell(players[4], captain == 4, Colors.yellow, context),
-        getStructuredGridCell(players[5], captain == 5 , Colors.yellow, context),
-        getStructuredGridCell(players[6], captain == 6,Colors.grey, context),
-      ],
-      
-    );
-  }
 }
 
 
