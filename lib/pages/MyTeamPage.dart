f7c402eaf5f1f6e1829b1446b626490a3210e2c6
Sam Owen-Hughes <samuelowenhughes@gmail.com>
2019-07-08 22:34:21 +0100

    first firestore read added

--- lib/pages/MyTeamPage.dart
+++ lib/pages/MyTeamPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 enum Response { Captain, Substitute, Stats, Transfer }
 
@@ -7,28 +8,15 @@ class MyTeamPage extends StatefulWidget{
 }
 
 class MyTeamPageState extends State<MyTeamPage> {
-
-   //Filler Data, Will need to be loaded from DB
-  List teamPlayers = ["J.Adshead", "Cabbage", "Horgan", "Montgomery", "SOH", "Bond", "TB3"];
-  List teamPoints = [-2, 4, 2, 5, 8, 6, 15];
-  int captain = 2;
-  int week = 50;
-  int total = 120;
-
   ButtonTheme getStructuredGridCell(player, points, captain, color, context) {
     String display = player;
     // Special treatment for captain
     if(captain){
-      // Display
-      //TODO - Decide which of these looks better
-        //display = player + "\n(Captain)";
-        //I prefer this
-        display = player + " (C)";
-
-        //Calculate extra points
-        //if (triple captain)
-        //points * 3
-        points = 2 * points;
+      display = player + " (C)";
+      //Calculate extra points
+      //if (triple captain)
+      //points * 3
+      points = 2 * points;
     }
 
     //Show weekly score
@@ -95,74 +83,85 @@ class MyTeamPageState extends State<MyTeamPage> {
         });
   }
 
+  Widget _buildDisplay(BuildContext context, DocumentSnapshot doc){
+  //call data
+    List teamPlayers = doc['players'];
+    List<int> teamPoints = [-2, 4, 2, 5, 8, 6, 15];
+    int captain = 2;
+    int week = 50;
+    int total = 120;
 
-  @override
-  Widget build(BuildContext context) {
     return SafeArea(
-        child: SizedBox(
-          height: double.infinity,
-          child: ListView(         
-          primary: true,
-          padding: const EdgeInsets.all(5.5),
-          children: <Widget>[
-            
-            //Transfer and points info
-            SizedBox(
-              height: 100,
-              child: Card(
-                color: Colors.purple,
-                child: Center(
-                   child: 
-                    Text("GW:" + week.toString()+ "\nTotal:" + total.toString(),
-                      style: TextStyle(fontSize: 30),
-                      textAlign: TextAlign.center,
-                    )
-                  ,),
-              ),
-            ),
-            
-
-            // Row for each position in team
-            Row(
-              children: <Widget>[
-                getStructuredGridCell(teamPlayers[0], teamPoints[0], captain == 0, Colors.red, context),
-                getStructuredGridCell(teamPlayers[1], teamPoints[1], captain == 1, Colors.red, context),
-              ],
-            ),
-            
-            Row(
-              children: <Widget>[
-                getStructuredGridCell(teamPlayers[2], teamPoints[2], captain == 2, Colors.orange, context),
-                getStructuredGridCell(teamPlayers[3], teamPoints[3], captain == 3, Colors.orange, context),
-              ],
-            ),
-
-            Row(
-              children: <Widget>[
-                getStructuredGridCell(teamPlayers[4], teamPoints[4], captain == 4, Colors.yellow, context),
-                getStructuredGridCell(teamPlayers[5], teamPoints[5], captain == 5, Colors.yellow, context),
-              ],
-            ),              
-            
-            // Substitute
-            Container( 
+      child: ListView(
+        children:<Widget>[
+          //Transfer and points info
+          SizedBox(
+            height: 100,
+            child: Card(
               color: Colors.purple,
-              child: getStructuredGridCell(teamPlayers[6], teamPoints[6], false, Colors.grey, context)
+              child: Center(
+                child: 
+                  Text("GW:" + week.toString()+ "\nTotal:" + total.toString(),
+                    style: TextStyle(fontSize: 30),
+                    textAlign: TextAlign.center,
+                  )
+                ,),
             ),
-            
-            //Boosts
-            SizedBox(
-              height: 50,
-              child: Card(
-                color: Colors.purple,
-                child: Center( child: Text("Boosts", style: TextStyle(fontSize: 30),),),
-              ),
+          ),
+          
+          // Row for each position in team
+          Row(
+            children: <Widget>[
+              getStructuredGridCell(teamPlayers[0], teamPoints[0], captain == 0, Colors.red, context),
+              getStructuredGridCell(teamPlayers[1], teamPoints[1], captain == 1, Colors.red, context),
+            ],
+          ),
+          
+          Row(
+            children: <Widget>[
+              getStructuredGridCell(teamPlayers[2], teamPoints[2], captain == 2, Colors.orange, context),
+              getStructuredGridCell(teamPlayers[3], teamPoints[3], captain == 3, Colors.orange, context),
+            ],
+          ),
+
+          Row(
+            children: <Widget>[
+              getStructuredGridCell(teamPlayers[4], teamPoints[4], captain == 4, Colors.yellow, context),
+              getStructuredGridCell(teamPlayers[5], teamPoints[5], captain == 5, Colors.yellow, context),
+            ],
+          ),              
+          
+          // Substitute
+          Container( 
+            color: Colors.purple,
+            child: getStructuredGridCell(teamPlayers[6], teamPoints[6], false, Colors.grey, context)
+          ),
+          
+          //Boosts
+          SizedBox(
+            height: 50,
+            child: Card(
+              color: Colors.purple,
+              child: Center( child: Text("Boosts", style: TextStyle(fontSize: 30),),),
             ),
-          ]  
-        )
+          ),
+
+        ]
       )
     );
   }
-}
 
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Firestore.instance.collection('testTeam').snapshots(),
+      builder: (context, snapshot){
+        if(!snapshot.hasData) {return const Text('Loading...');}
+        return Builder(
+          builder: (BuildContext context) {return _buildDisplay(context, snapshot.data.documents[0]);}
+        );        
+      } 
+    );
+  }
+}
 
