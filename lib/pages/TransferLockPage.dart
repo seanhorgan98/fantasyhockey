931ac1aac5d9a64ee9bccaba98a329d6d44456af
Sam Owen-Hughes <samuelowenhughes@gmail.com>
2019-08-22 23:40:24 +0100

    Finished stabilising last points updating commands

--- lib/pages/TransferLockPage.dart
+++ lib/pages/TransferLockPage.dart
@@ -48,11 +48,130 @@ class TransferLockPage extends StatelessWidget {
               onPressed: () => getData(unlimit),
             )
           ),
-        ],
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 2,
+            fit: FlexFit.tight,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              margin: new EdgeInsets.symmetric(horizontal: 30, vertical: 7),
+              height: 50,
+              child: MaterialButton(
+                minWidth: MediaQuery.of(context).size.width,
+                color: Theme.of(context).accentColor,
+                onPressed: endGW,
+                child: Text("End GW", style: TextStyle(fontSize: 18, fontFamily: 'Titillium')),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 2,
+            fit: FlexFit.tight,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              margin: new EdgeInsets.symmetric(horizontal: 30, vertical: 7),
+              height: 50,
+              child: MaterialButton(
+                minWidth: MediaQuery.of(context).size.width,
+                color: Theme.of(context).accentColor,
+                onPressed: startGW,
+                child: Text("Start GW", style: TextStyle(fontSize: 18, fontFamily: 'Titillium')),
+              ),
+            ),
+          )  
+        ]
       )
     );
       
   }
+
+  /*
+  First function gets data for section function to handle
+  Adds gw to total - only time total is changed
+  */
+  void endGW(){
+    Firestore.instance.collection('Teams').getDocuments().then( 
+      (snap) => eachUserEnd(snap)
+    );
+
+    Firestore.instance.collection('Players').getDocuments().then(
+      (snap) => eachEndPlayer(snap)
+    );
+  }
+
+  void eachEndPlayer(QuerySnapshot snapshot){
+    WriteBatch batch = Firestore.instance.batch();
+    for(DocumentSnapshot player in snapshot.documents){
+      if(player.documentID == 'No Player'){
+        continue;
+      }
+
+      int newTotal = player['totalPoints'] + player['gw'];
+
+      DocumentReference uRef = player.reference;
+      batch.updateData(uRef, {
+        "totalPoints": newTotal
+      });
+    }
+    batch.commit();
+  }
+
+  void eachUserEnd(QuerySnapshot snapshot){
+    for (DocumentSnapshot user in snapshot.documents){
+      if(user.documentID == '000'){
+        continue;
+      }
+      
+      List totalList = user['totals'];
+      var newList = [totalList[0], totalList[1] + totalList[0] ];
+      
+      //write
+      Firestore.instance.runTransaction( (transaction) async {
+        DocumentSnapshot freshSnap = await transaction.get(user.reference);
+        transaction.update(freshSnap.reference, {
+          'totals': newList
+        });
+      });
+    }
+  }
+
+
+  /*
+  First function gets data for section function to handle
+  sets gw to 0 
+  */
+  void startGW(){
+    Firestore.instance.collection('Players').getDocuments().then( 
+      (snap) => eachUserStart(snap)
+    );
+  }
+
+  void eachUserStart(QuerySnapshot snapshot){
+    WriteBatch batch = Firestore.instance.batch();
+    for (DocumentSnapshot player in snapshot.documents){
+      //Skip empty player
+      if(player.documentID == 'No Player'){
+        continue;
+      }
+      //reset player gw
+      DocumentReference uRef = player.reference;
+      batch.updateData(uRef, {
+        "gw": 0
+      });
+    }
+    //finish writes
+    batch.commit();
+  }
+
+
+//TODO stop being fancy lol
 
   //function to send all user profiles to setter function
   getData(Function set){
@@ -60,7 +179,6 @@ class TransferLockPage extends StatelessWidget {
       "transferSetting", isLessThan: 3).snapshots().take(1).forEach(
         (snapshot) => set(snapshot) );
   }
-
 
   /*
   Near identical functions passed by buttons
